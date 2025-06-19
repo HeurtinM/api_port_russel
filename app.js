da35e889579fa30ee4a538f713c417daff3e17e8
Heurtin Manu <manu.heurtin@gmail.com>
2025-06-19 12:20:33 +0200

    intallé nodemon et generator, ajouter 2 routes basiques

--- app.js
+++ app.js
@@ -2,7 +2,21 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.get('/', (req,res)=>{
+    res.type('text/plain')
+    res.send("page d'acceuil")
+});
 
+app.get('/catways',(req,res)=>{
+    res.type('text/plain')
+    res.send("page CRUD pour catways")
+});
+
+app.use((req, res)=>{
+    res.type('text/plain')
+    res.status(404)
+    res.send("404 page introuvable")
+});
 
 app.listen(port,() => {
     console.log('server app listening on port ' + port);
