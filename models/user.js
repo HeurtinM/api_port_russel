30cffa4461411b899916554541dacb5807927dcc
Heurtin Manu <manu.heurtin@gmail.com>
2025-07-10 12:19:18 +0200

    ajout de control de valeur pour users

--- models/user.js
+++ models/user.js
@@ -18,7 +18,16 @@ const User = new Schema({
     },
     password: {
         type: String,
-        trim: true,
+
+        required: [true, 'Le mot de passe est requis'],
+        maxLength: 30,
+        //verifie que le mdp ne contient pas d'espace. validate et validator trouver dans la doc mongoose https://mongoosejs.com/docs/validation.html et "/\s/g" sur stack overflow
+        validate:{
+            validator: function hasWhiteSpace(s) {
+            return !/\s/g.test(s);
+            },
+            message: props => `${props.value} le mot de passe ne doit pas contenir d'espace`
+        },
     },
 }, {
 
